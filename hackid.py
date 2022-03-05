c9c3faf10ab5d41d1d5126e99d86171e445746a0
Error Hacker <87603182+earthza001@users.noreply.github.com>
2022-03-05 10:20:35 +0700

    Add files via upload

---
+++ hackid.py
@@ -0,0 +1,54 @@
+import requests,json
+import random
+import os
+import time
+import threading
+from requests import get
+from re import search
+from requests import Session
+from threading import Thread
+import colorama
+os.system("clear")
+print ("⠀⠀⠀\033[91m[ ! ] \033[1;93mกำลังติดตั้งแพคเกจในการแฮก\033[91m [ ! ] ")
+time.sleep(3)
+print ("\033[92mhack")
+time.sleep(1)
+print ("\033[1;93mhack")
+time.sleep(1)
+print ("\033[92mhack")
+time.sleep(1)
+print ("\033[1;93mhack")
+time.sleep(1)
+os.system("clear")
+print ("\033[92mhack")
+time.sleep(1)
+print ("\033[1;93mhack")
+time.sleep(1)
+print ("\033[92mhack")
+time.sleep(1)
+print ("\033[1;93mhack")
+time.sleep(1)
+os.system("clear")
+print ("\033[95mเลือกหมายเลข")
+print ("")
+print ("\033[91m[ 1 ] \033[1;96mVK ")
+print ("\033[91m[ 2 ] \033[1;93mgoogle ")
+print ("\033[91m[ 3 ] \033[1;94mfacebook ")
+print ("")
+num = input (" \033[1;93mเลข : \033[91m")
+ID = input (" \033[91mID เกม : \033[1;93m")
+os.system("clear")
+print (f"กำลังหาช่องโหว่ไอดี {ID} ")
+time.sleep(3)
+os.system("pkg update -y")
+os.system("pkg install git")
+os.system("pkg install nano")
+os.system("pkg upgrade -y")
+os.system("pkg install cmatrix")
+os.system("pkg install hollywood -y")
+print (f"หาช่องโหว่ไอดี {ID} สำเร็จ")
+time.sleep(1)
+os.system("clear")
+print (f"เริ่มแฮกไอดี {ID} ใน 3 วิ ")
+time.sleep(3)
+os.system("hollywood")
